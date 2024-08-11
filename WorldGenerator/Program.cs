7060f0861f67789b8c0a774224bbcaa93d99c1f4
KaisoBits <31374466+KaisoBits@users.noreply.github.com>
2024-08-11 12:37:27 +0200

    Add World utilities

--- WorldGenerator/Program.cs
+++ WorldGenerator/Program.cs
@@ -9,6 +9,7 @@ const int worldY = 30;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder();
 builder.Services.AddScoped(sp => World.CreateWorld(worldX, worldY));
+builder.Services.AddScoped<WorldUtilities>();
 builder.Services.AddScoped<EventBus>();
 builder.Services.AddScoped<ConsoleInterface>();
 builder.Services.AddScoped<Terrain>();
---
+++ WorldGenerator/WorldUtilities.cs
@@ -0,0 +1,37 @@
+﻿namespace WorldGenerator;
+
+public class WorldUtilities
+{
+    private readonly World _world;
+
+    public WorldUtilities(World world)
+    {
+        _world = world;
+    }
+
+    public List<IEntity> GetAll(Predicate<IEntity> predicate)
+    {
+        return _world.Entities.Where(w => predicate(w)).ToList();
+    }
+
+    public List<IEntity> GetEntitiesInRange(Vector position, Vector rectangle)
+    {
+        List<IEntity> entities = [];
+        for (int y = position.Y - rectangle.Y; y < position.Y + rectangle.Y; y++)
+        {
+            for (int x = position.X - rectangle.X; x < position.X + rectangle.X; x++)
+            {
+                entities.AddRange(_world[x, y].Contents);
+            }
+        }
+
+        return entities;
+    }
+
+    public IEntity? GetClosest(Vector startPos, Predicate<IEntity> predicate)
+    {
+        return _world.Entities
+            .OrderBy(e => (e.Position - startPos).SimpleLen())
+            .FirstOrDefault(e => predicate(e));
+    }
+}
